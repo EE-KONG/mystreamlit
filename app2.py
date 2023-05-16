556716473d5fc3a7f294d2be0058a1f069a2d2f3
EE-KONG <126738970+EE-KONG@users.noreply.github.com>
2023-05-16 12:48:51 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,5 +3,5 @@ import numpy as np
 import pandas as pd
 
 st.title("Map")
-df=pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon']])
+df=pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
 st.map(df)
