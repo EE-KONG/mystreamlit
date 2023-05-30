3542ea68c914d1fae535564672605ef36f423c94
EE-KONG <126738970+EE-KONG@users.noreply.github.com>
2023-05-30 12:38:00 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 x=st.slider("WoW",10,50)
-st.button("버튼을 누르세요.")
-if x<20: st.write("C")
-elif x<40: st.write("B")
-else: st.write("A")
+b=st.button("버튼을 누르세요.")
+if b==True:
+  if x<20: st.write("C")
+  elif x<40: st.write("B")
+  else: st.write("A")
