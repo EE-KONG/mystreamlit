f8e5a395a62d6ec29beb6e2ef8890553d8e068c1
EE-KONG <126738970+EE-KONG@users.noreply.github.com>
2023-05-30 12:48:24 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
+d=st.date_input("생일")
 x=st.slider("WoW",10,50)
 b=st.button("버튼을 누르세요.")
 if b==True:
   if x<20: st.write("C")
   elif x<40: st.write("B")
   else: st.write("A")
+if d=="2022/12/15": st.write("WoW")
